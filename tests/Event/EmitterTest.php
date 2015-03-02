d12b8f15916007151249aa03e3c09f7863a2d29c
Tom Morton <thomas.morton@thesalegroup.co.uk>
2015-03-02 23:25:09 +0000

    complete test coverage?

--- tests/Event/EmitterTest.php
+++ tests/Event/EmitterTest.php
@@ -23,8 +23,20 @@ class EmitterTest extends PHPUnit_Framework_TestCase
         );
     }
 
+    public function emitErrorDataProvider()
+    {
+        return array(
+            array(null),
+            array('noncallable'),
+            array(1),
+            array($this),
+            array(false)
+        );
+    }
+
     public function _callback($event) 
     {
+        $this->assertInstanceOf('\Moxy\Event', $event);
         $this->assertEquals('test.event', $event->name);
         $this->assertEquals('data', $event->test);
     }
@@ -39,6 +51,52 @@ class EmitterTest extends PHPUnit_Framework_TestCase
         $this->assertInternalType('array',$events);
         $this->assertCount(1,$events);
         $this->assertInstanceOf('\Moxy\Event\Dispatcher',$events['test.event']);
+    }
+
+    /**
+     * @depends testOn
+     */
+    public function testListen()
+    {
+        $this->emitter->listen('test.listen',array($this, '_callback'));
+
+        $eventsProp = $this->inspect->getProperty('_events');
+        $eventsProp->setAccessible(true);
+        $events = $eventsProp->getValue($this->emitter);
+        $this->assertInternalType('array',$events);
+        $this->assertCount(2,$events);
+        $this->assertInstanceOf('\Moxy\Event\Dispatcher',$events['test.listen']);
+    }
+
+    /**
+     * @depends testOn
+     */
+    public function testEmit()
+    {
+        $this->emitter->emit('test.event',array('test' => 'data'));
+    }
+
+
+    /**
+     * @depends testOn
+     * @dataProvider errorDataProvider
+     * @expectedException Exception
+     * @expectedExceptionMessage Event callback must be a callable
+     */
+    public function testOnNonCallable($callable)
+    {
+        $this->emitter->on('test.event',$callable);
+    }
+
+     /**
+     * @depends testEmit
+     * @dataProvider emitErrorDataProvider
+     * @expectedException Exception
+     * @expectedExceptionMessage Moxy\Event data must be an array
+     */
+    public function testEmitWithInvalid($data)
+    {
+        $this->emitter->emit('test.event',$data);
     }
 
 
